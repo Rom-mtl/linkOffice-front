b165426927ee33f817f053b003d34fff4b6bf911
Guillem <guillem.dardill@gmail.com>
2021-01-14 16:08:29 +0100

    player moveable on the map

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,27 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
+import produce from 'immer';
 import './Board.css';
 
+const boardStyle = {
+  width: '100%',
+  height: '800px',
+  display: 'grid',
+  gridTemplateColumns: 'repeat(12, 1fr)',
+  gridAutoRows: 'auto',
+  gridTemplateRows: 'repeat(14, 1fr)',
+  marginBottom: '10px',
+  marginTop: '11px',
+  marginLeft: '11px',
+  position: 'absolute',
+  zIndex: '2',
+};
+
+const squareBoard = {
+  border: 'none',
+};
+
+const board = [];
+for (let i = 0; i < 168; i += 1) {
+  board.push({ id: i, content: '', currentPlayer: false });
+}
+board[0].currentPlayer = true;
+
 const Board = () => {
+  const [myBoard, setmyBoard] = useState(board);
+  const [playerPosition, setPlayerPosition] = useState(0);
+
+  const changeSquareContent = (id) => {
+    const nextState = produce(myBoard, (draftState) => {
+      // mettre à jour current player de l'ancienne case à false
+      // mettre à jour le state du player position
+      // mettre à jour current player de l'id en cours
+      // eslint-disable-next-line no-param-reassign
+      draftState[playerPosition].currentPlayer = false;
+      setPlayerPosition(id);
+      // eslint-disable-next-line no-param-reassign
+      draftState[id].currentPlayer = true;
+    });
+    setmyBoard(nextState);
+  };
+
   return (
-    <div className="wrapper">
-      <div className="one" />
-      <div className="two" />
-      <div className="three" />
-      <div className="four" />
-      <div className="five" />
-      <div className="six" />
-      <div className="seven" />
-      <div className="height" />
-      <div className="nine" />
-      <div className="ten" />
-      <div className="eleven" />
-      <div className="douze" />
-      <div className="treize" />
-      <div className="conf" />
-      <div className="cafe" />
-      <div className="canap" />
-      <div className="tapis" />
-      <div className="planteDeux" />
+    <div className="board-body">
+      <div style={boardStyle} className="board">
+        {myBoard.map((square, index) => {
+          return (
+            <div
+              style={squareBoard}
+              // eslint-disable-next-line react/no-array-index-key
+              key={index}
+              onKeyPress={() => {}}
+              role="button"
+              tabIndex="0"
+              onClick={() => {
+                changeSquareContent(square.id);
+              }}
+            >
+              {square.currentPlayer ? (
+                <div className="playerPosition" />
+              ) : (
+                <div />
+              )}
+            </div>
+          );
+        })}
+      </div>
+      <div className="wrapper">
+        <div className="one" />
+        <div className="two" />
+        <div className="three" />
+        <div className="four" />
+        <div className="five" />
+        <div className="six" />
+        <div className="seven" />
+        <div className="height" />
+        <div className="nine" />
+        <div className="ten" />
+        <div className="eleven" />
+        <div className="douze" />
+        <div className="treize" />
+        <div className="conf" />
+        <div className="cafe" />
+        <div className="canap" />
+        <div className="tapis" />
+        <div className="planteDeux" />
+      </div>
     </div>
   );
 };
