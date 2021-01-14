419292d5a67f97aa4d3f18d29efab90e71ddab3b
Guillem <guillem.dardill@gmail.com>
2021-01-14 18:33:47 +0100

    link pseudo between home page and board page + pseudo seen on the board

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,14 +2,25 @@ import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
 
-const Home = () => {
+const Home = (props) => {
+  const { pseudo, setPseudo } = props;
+
+  const changePseudo = (e) => {
+    setPseudo(e.target.value);
+  };
+
   return (
     <div className="backgroundHome">
       <div className="backTitle">
         <div className="gif" />
         <div className="logo" />
         <h2 className="titleHome">Le bureau à la maison !</h2>
-        <input type="text" id="inputEnter" />
+        <input
+          type="text"
+          id="inputEnter"
+          value={pseudo}
+          onChange={changePseudo}
+        />
         <Link exact to="/board">
           <button type="button" label="Prenom" className="buttonEnter">
             BADGER
