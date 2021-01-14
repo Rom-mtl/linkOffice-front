e42aa47c37a9b8fef941ccfa1ebcba44f37c3446
AmandineGervis <gervis.amandine@gmail.com>
2021-01-14 12:02:12 +0100

    front better

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,7 +1,23 @@
-import React from 'react';
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React, { useState } from 'react';
 import './Board.css';
 
 const Board = () => {
+  const [isFiltered, setIsFiltered] = useState(true);
+  const [isWrite, setIsWrite] = useState(true);
+  const [isOnline, setIsOnline] = useState(true);
+
+  const handlerFilter = () => {
+    setIsFiltered(!isFiltered);
+  };
+
+  const handlerWrite = () => {
+    setIsWrite(!isWrite);
+  };
+
+  const handlerOnLine = () => {
+    setIsOnline(!isOnline);
+  };
   return (
     <div className="wrapper">
       <div className="one" />
@@ -17,11 +33,33 @@ const Board = () => {
       <div className="eleven" />
       <div className="douze" />
       <div className="treize" />
-      <div className="conf" />
+      <button
+        label="text"
+        className={!isWrite ? 'conf' : 'conf2'}
+        type="button"
+        onClick={handlerWrite}
+      />
       <div className="cafe" />
       <div className="canap" />
       <div className="tapis" />
-      <div className="planteDeux" />
+      <div className="murTop" />
+      <div className="murBottom" />
+      <div className="murLeft" />
+      <div className="murRight" />
+      <div className="cafePointer" />
+      <div className="gamePointer" />
+      <button
+        label="text"
+        className={!isFiltered ? 'planteDeux' : 'planteFane'}
+        type="button"
+        onClick={handlerFilter}
+      />
+      <button
+        label="text"
+        className={!isOnline ? 'thomas' : 'Thomas2'}
+        type="button"
+        onClick={handlerOnLine}
+      />
     </div>
   );
 };
