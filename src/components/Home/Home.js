6287f189fa0b6e73b20e5deffb835069ca014178
AmandineGervis <gervis.amandine@gmail.com>
2021-01-14 16:14:19 +0100

    page home ok

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,14 +5,17 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className="backgroundHome">
-      <div className="logo" />
-      <h2 className="titleHome">Le bureau à la maison !</h2>
-      <input type="text" id="inputEnter" />
-      <Link exact to="/board">
-        <button type="button" label="Prenom" className="buttonEnter">
-          BADGER
-        </button>
-      </Link>
+      <div className="backTitle">
+        <div className="gif" />
+        <div className="logo" />
+        <h2 className="titleHome">Le bureau à la maison !</h2>
+        <input type="text" id="inputEnter" />
+        <Link exact to="/board">
+          <button type="button" label="Prenom" className="buttonEnter">
+            BADGER
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
